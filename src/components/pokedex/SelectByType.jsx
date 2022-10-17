4b37d3d2cbc623f0c0bba392cf4b83ba0bedb023
Stefa093 <estefaniasalazar093@gmail.com>
2022-10-16 20:05:59 -0500

    1.3

--- src/components/pokedex/SelectByType.jsx
+++ src/components/pokedex/SelectByType.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import '../pokedex/styles/SelectByType.css'
+import '../../components/pokedex/styles/selectByType.css'
 
 const SelectByType = ({setTypeSelected}) => {
   const [types, setTypes] = useState();
